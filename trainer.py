230f405693695122c7e8aef6205a1278f3830778
Gael Reinaudi <gael.reinaudi@gmail.com>
2017-07-10 01:47:55 -0400

    correct losses

--- trainer.py
+++ trainer.py
@@ -123,8 +123,8 @@ class Trainer(object):
     def build_model(self):
         x = self.x
         y = self.y
-        print('x:',x)
-        print('y:',y)
+        print('x:', x)
+        print('y:', y)
 
         net = x
         nLayPrev = self.data_loader.n_input
@@ -135,13 +135,14 @@ class Trainer(object):
             net = nn_layer(net, nLayPrev, nLay, 'layer'+str(iLay))
             nLayPrev = nLay
         pred = nn_layer(net, nLayPrev, self.data_loader.n_output, 'layerout', act=tf.identity)
+        print('pred:', pred)
 
         # Add ops to save and restore all the variables.
         with tf.name_scope('loss'):
-            self.loss = tf.reduce_mean(tf.square(pred - y))
+            self.loss = tf.losses.mean_squared_error(y, pred)
 
         with tf.name_scope('logloss'):
-            self.logloss = tf.log(10**-18 + tf.reduce_mean(tf.square(pred - y))) # add a tiny bias to avoid numerical error
+            self.logloss = tf.log(1E-18 + self.loss) / tf.log(10.0) # add a tiny bias to avoid numerical error
 
         self.summary_op = tf.summary.merge([
             tf.summary.histogram("x", self.x),
