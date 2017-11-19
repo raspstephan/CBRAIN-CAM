3ab5a44e4e8ca8bfcb33ee4b67aef73e4e742776
S.Rasp <s.rasp@lmu.de>
2017-11-19 10:37:41 +0100

    Added save weights script, renamed extended output nb and added mse analysis

---
+++ keras/save_weights.py
@@ -0,0 +1,37 @@
+"""Little script to save weights from saved model.
+
+Author: Stephan Rasp
+"""
+
+from keras.models import load_model
+from configargparse import ArgParser
+from losses import *
+from keras.utils.generic_utils import get_custom_objects
+metrics_dict = dict([(f.__name__, f) for f in metrics])
+get_custom_objects().update(metrics_dict)
+
+def main(inargs):
+    """Load saved model and save weights
+
+    Args:
+        inargs: Namespace
+
+    """
+
+    model = load_model(inargs.model_path)
+
+    model.save_weights(inargs.save_path)
+
+
+if __name__ == '__main__':
+
+    p = ArgParser()
+    p.add_argument('--model_path',
+                   type=str,
+                   help='Path to model')
+    p.add_argument('--save_path',
+                   type=str,
+                   help='Path for saved weights.')
+
+    args = p.parse_args()
+    main(args)
