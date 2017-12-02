cdfbd902cf44f65799e4e8e5d6a01bacbb9e3901
S.Rasp <s.rasp@lmu.de>
2017-12-02 07:43:51 +0100

    Added fast shuffling method

--- data_processing/shuffle_ds.py
+++ data_processing/shuffle_ds.py
@@ -11,6 +11,7 @@ import sys
 from datetime import datetime
 from subprocess import getoutput
 
+global verbose
 
 def create_log_str():
     """Create a log string to add to the netcdf file for reproducibility.
@@ -40,17 +41,54 @@ def create_log_str():
     return log_str
 
 
+def fast(features_orig, targets_orig, features_shuffle, targets_shuffle,
+         chunk_size):
+    # Try shuffling in batches that fit into RAM
+    n_samples = features_orig.dimensions['sample'].size
+    n_chunks = int(np.ceil(n_samples / float(chunk_size)))
+
+    for i in range(n_chunks):
+        print('Chunk %i out of %i' % (i+1, n_chunks))
+        start_idx = i * chunk_size
+        stop_idx = np.min([(i+1) * chunk_size, n_samples])
+        rand_idxs = np.arange(stop_idx - start_idx)
+        np.random.shuffle(rand_idxs)
+        for orig, shuffle, name in zip([features_orig, targets_orig],
+                                       [features_shuffle, targets_shuffle],
+                                       ['features', 'targets']):
+            if verbose > 0: print('Loading %s chunk into RAM' % name)
+            chunk = orig.variables[name][start_idx:stop_idx]
+
+            if verbose > 0: print('Shuffling chunk')
+            chunk = chunk[rand_idxs]
+
+            if verbose > 0: print('Writing chunk')
+            shuffle.variables[name][start_idx:stop_idx] = chunk
+
+
+def meticulous(features_orig, targets_orig, features_shuffle, targets_shuffle):
+
+    # Create random indices
+    rand_idxs = np.arange(features_orig.dimensions['sample'].size)
+    np.random.shuffle(rand_idxs)
+
+    # Write data in random order
+    for i in tqdm(range(rand_idxs.shape[0])):
+        i_rand = rand_idxs[i]
+        features_shuffle.variables['features'][i] = \
+            features_orig.variables['features'][i_rand]
+        targets_shuffle.variables['targets'][i] = \
+            targets_orig.variables['targets'][i_rand]
+
+
 def main(inargs):
+    np.random.seed(inargs.random_seed)
+
     # Open original files
     print('Reading files:', inargs.pref + '_features.nc',
           inargs.pref + '_targets.nc')
     features_orig = Dataset(inargs.pref + '_features.nc', 'r')
     targets_orig = Dataset(inargs.pref + '_targets.nc', 'r')
-
-    # Create random indices
-    np.random.seed(inargs.random_seed)
-    rand_idxs = np.arange(features_orig.dimensions['sample'].size)
-    np.random.shuffle(rand_idxs)
 
     # Create equivalent new files
     print('Creating files:', inargs.pref + '_features_shuffle.nc',
@@ -68,17 +106,25 @@ def main(inargs):
         shuffle.log = ('Original log:\n' + orig.log + '\nNew log:\n' +
                        create_log_str())
 
-    # Write data in random order
-    for i in tqdm(range(rand_idxs.shape[0])):
-        i_rand = rand_idxs[i]
-        features_shuffle.variables['features'][i] = \
-            features_orig.variables['features'][i_rand]
-        targets_shuffle.variables['targets'][i] = \
-            targets_orig.variables['targets'][i_rand]
+    if inargs.method == 'meticulous':
+        meticulous(features_orig, targets_orig, features_shuffle,
+                   targets_shuffle)
+    elif inargs.method == 'fast':
+        fast(features_orig, targets_orig, features_shuffle, targets_shuffle,
+             inargs.chunk_size)
+    else:
+        raise Exception
 
+    features_shuffle.close()
+    targets_shuffle.close()
+    features_orig.close()
+    targets_orig.close()
 
 if __name__ == '__main__':
     p = ArgParser()
+    p.add_argument('--method',
+                   type=str,
+                   help='[Meticulous or fast]')
     p.add_argument('--pref',
                    type=str,
                    help='Prefix. ie without the _features.nc')
@@ -86,5 +132,16 @@ if __name__ == '__main__':
                    type=int,
                    default=42,
                    help='Random seed for shuffling of data.')
+    p.add_argument('--chunk_size',
+                   type=int,
+                   default=None,
+                   help='Size of chunks for fast method')
+    p.add_argument('--verbose',
+                   type=int,
+                   default=0,
+                   help='Verbosity level')
     args = p.parse_args()
+
+    verbose = args.verbose
+
     main(args)
