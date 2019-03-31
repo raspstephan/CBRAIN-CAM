91961ab5bb59de6120179f3f6e3d110290fd9f07
S.Rasp <s.rasp@lmu.de>
2019-03-31 11:44:12 +0200

    Implemented Toms conservation layers but last one is still producing wrong results

--- cbrain/cam_constants.py
+++ cbrain/cam_constants.py
@@ -8,6 +8,7 @@ Author: Stephan Rasp, raspstephan@gmail.com
 DT = 1800.
 L_V = 2.501e6   # Latent heat of vaporization
 L_I = 3.337e5   # Latent heat of freezing
+L_F = L_I
 L_S = L_V + L_I # Sublimation
 C_P = 1.00464e3 # Specific heat capacity of air at constant pressure
 G = 9.80616
