70cedd945f70c02a3e9dfad6e2284c125662da5b
tbeucler <tom.beucler@gmail.com>
2019-04-23 05:35:11 +0200

    Figure 1a of conserving network paper

---
+++ notebooks/tbeucler_devlog/010_Python_004.py
@@ -0,0 +1,47 @@
+# tgb - 4/22/2019 - Calculating residual statistics for architecture-contrained neural network
+# tgb - 4/19/2019 - The goal is to make a slurm-callable script to calculate the statistics and residuals of all the paper neural networks over the validation dataset. This script is specialized to the +0K experiment.
+
+from cbrain.imports import *
+from cbrain.data_generator import *
+from cbrain.cam_constants import *
+from cbrain.losses import *
+from cbrain.utils import limit_mem
+from cbrain.layers import *
+import tensorflow as tf
+import tensorflow.math as tfm
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+import xarray as xr
+import numpy as np
+from cbrain.model_diagnostics import ModelDiagnostics
+# Otherwise tensorflow will use ALL your GPU RAM for no reason
+limit_mem()
+TRAINDIR = '/local/Tom.Beucler/SPCAM_PHYS/'
+DATADIR = '/project/meteo/w2w/A6/S.Rasp/SP-CAM/fluxbypass_aqua/'
+PREFIX = '8col009_01_'
+
+import os
+os.chdir('/filer/z-sv-pool12c/t/Tom.Beucler/SPCAM/CBRAIN-CAM')
+
+config_fn = '/filer/z-sv-pool12c/t/Tom.Beucler/SPCAM/CBRAIN-CAM/pp_config/8col_rad_tbeucler_local_PostProc.yml'
+data_fn = '/local/Tom.Beucler/SPCAM_PHYS/8col009_01_valid.nc'
+dict_lay = {'SurRadLayer':SurRadLayer,'MassConsLayer':MassConsLayer,'EntConsLayer':EntConsLayer}
+
+# 1) Load model
+path = TRAINDIR+'HDF5_DATA/NNA.h5'
+NN = load_model(path,custom_objects=dict_lay)
+
+# 2) Define model diagnostics object
+md = ModelDiagnostics(NN,config_fn,data_fn)
+
+# 3) Calculate statistics and save in pickle file
+# md.compute_stats()
+# path = TRAINDIR+'HDF5_DATA/NNAmd.pkl'
+# pickle.dump(md.stats,open(path,'wb'))
+# print('Stats are saved in ',path)
+
+# 4) Calculate budget residuals and save in pickle file
+md.compute_res()
+path = TRAINDIR+'HDF5_DATA/NNAres.pkl'
+pickle.dump(md.res,open(path,'wb'))
+print('Budget residuals are saved in ',path)
